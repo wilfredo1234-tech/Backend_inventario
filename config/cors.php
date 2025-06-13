ff444f942ffb27ffb9f80a9b40f65b3719ff654e
wilfredo <palaciojimenezw@gmail.com>
2025-06-13 11:22:03 -0500

    conectando frontend de produccion

--- config/cors.php
+++ config/cors.php
@@ -11,9 +11,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173', 
-    ],
+   'allowed_origins' => [
+    'http://localhost:5173',
+    'https://inventario-lemon.vercel.app',
+],
+
 
     'allowed_origins_patterns' => [],
 
